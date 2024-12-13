b489f0a5dd8d47edbb7ae326ea9a80eefa3042a0
Esteban Flores <e2002florespulido@gmail.com>
2024-12-13 11:08:59 -0600

    Modifying package.json

--- backend/server.js
+++ backend/server.js
@@ -37,7 +37,7 @@ if (process.env.NODE_ENV === "production") {
   })
 }
 
-app.listen(8000, () => {
+app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`)
   connectMongoDB()
 })
